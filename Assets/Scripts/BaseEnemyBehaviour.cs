18c1d4d228902b75bb60c679e3ab0ba28c0564f7
stathampen <33280323+stathampen@users.noreply.github.com>
2020-10-12 11:46:56 +0100

    enemy base class now targets the player when they are within a certain radius

--- Assets/Scripts/BaseEnemyBehaviour.cs
+++ Assets/Scripts/BaseEnemyBehaviour.cs
@@ -9,6 +9,7 @@ public class BaseEnemyBehaviour : MonoBehaviour
     [SerializeField] private GameObject sunTarget;
     [SerializeField] private GameObject playerTarget;
     [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private float distantToTargetPlayer = 4f;
 
     // Start is called before the first frame update
     void Start()
@@ -36,6 +37,28 @@ public class BaseEnemyBehaviour : MonoBehaviour
     void Update()
     {
         //the enemy should go for the sun initially
-        agent.SetDestination(sunTarget.transform.position);
+        checkPlayerDistance();
+    }
+
+    private void checkPlayerDistance()
+    {
+        //find out how far the player is from the enemy
+        float playerDistance = Vector3.Distance (transform.position, playerTarget.transform.position);
+
+        //If the player is close enough then target them
+        if ( playerDistance < distantToTargetPlayer)
+        {
+            agent.SetDestination(playerTarget.transform.position);
+        }
+        else
+        {
+            agent.SetDestination(sunTarget.transform.position);
+        }
+        
+    }
+
+    private void OnDrawGizmos() {
+        //for testing show the area when the enemy will target the player
+        Gizmos.DrawSphere(transform.position, distantToTargetPlayer);
     }
 }
