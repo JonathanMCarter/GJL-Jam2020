59763d98e2b14f92af6e040e055f573d14b3755f
Jonathan <jonathan@carter.games>
2020-10-12 12:22:01 +0100

    Gnome Movement & Attacks
    Gnome stuff.

---
+++ Assets/Scripts/Gnome/FireworkMove.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+/*
+*  Copyright (c) Jonathan Carter
+*  E: jonathan@carter.games
+*  W: https://jonathan.carter.games/
+*/
+
+namespace DresslikeaGnome.OhGnomes
+{
+    public class FireworkMove : MonoBehaviour
+    {
+        [SerializeField] private GameObject fireworkObject;
+        [SerializeField] private GameObject fireworkPrefab;
+        [Range(30,100)]
+        [SerializeField] private float fireworkSpeed = 50f;
+        [SerializeField] private float weaponCooldown = .75f;
+
+        private bool isCoR;
+        private List<GameObject> fireworkPool;
+        private GnomeAttacks attacks;
+        private WaitForSeconds wait;
+
+
+        private void OnEnable()
+        {
+            wait = new WaitForSeconds(weaponCooldown);
+            attacks = GetComponent<GnomeAttacks>();
+            fireworkObject.SetActive(false);
+        }
+
+
+        private void Awake()
+        {
+            fireworkPool = new List<GameObject>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                GameObject _go = Instantiate(fireworkPrefab);
+                _go.name = "* (Pool) Firework Missile *";
+                _go.SetActive(false);
+                fireworkPool.Add(_go);
+            }
+        }
+
+
+        private void Update()
+        {
+            if (attacks.GetActiveWeapon().Equals(GnomeWeapons.Firework) && !fireworkObject.activeSelf)
+            {
+                fireworkObject.SetActive(true);
+            }
+            else if (!attacks.GetActiveWeapon().Equals(GnomeWeapons.Firework) && fireworkObject.activeSelf)
+            {
+                fireworkObject.SetActive(false);
+            }
+        }
+
+
+        public void UseAbility()
+        {
+            if (!isCoR)
+            {
+                StartCoroutine(FireworkAbilityCo());
+            }
+        }
+
+
+        private IEnumerator FireworkAbilityCo()
+        {
+            isCoR = true;
+
+            GameObject _go = GetFireworkMissile();
+            _go.transform.position = fireworkObject.transform.position;
+            _go.transform.rotation = fireworkObject.transform.rotation;
+            _go.GetComponent<Rigidbody>().velocity += transform.forward * fireworkSpeed;
+            _go.SetActive(true);
+
+            yield return wait;
+
+            isCoR = false;
+        }
+
+
+        private GameObject GetFireworkMissile()
+        {
+            for (int i = 0; i < fireworkPool.Count; i++)
+            {
+                if (!fireworkPool[i].activeInHierarchy)
+                {
+                    return fireworkPool[i];
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Gnome/FishingRodMove.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+/*
+*  Copyright (c) Jonathan Carter
+*  E: jonathan@carter.games
+*  W: https://jonathan.carter.games/
+*/
+
+namespace DresslikeaGnome.OhGnomes
+{
+    public class FishingRodMove : MonoBehaviour
+    {
+        [SerializeField] private GameObject fishingRodObject;
+        private GnomeAttacks attacks;
+
+        private void OnEnable()
+        {
+            attacks = GetComponent<GnomeAttacks>();
+            fishingRodObject.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (attacks.GetActiveWeapon().Equals(GnomeWeapons.FishingRod) && !fishingRodObject.activeSelf)
+            {
+                fishingRodObject.SetActive(true);
+            }
+            else if (!attacks.GetActiveWeapon().Equals(GnomeWeapons.FishingRod) && fishingRodObject.activeSelf)
+            {
+                fishingRodObject.SetActive(false);
+            }
+        }
+
+        public void UseAbility()
+        {
+            // would play an animation, for now its gonna move to a position and do something
+        }
+    }
+}
