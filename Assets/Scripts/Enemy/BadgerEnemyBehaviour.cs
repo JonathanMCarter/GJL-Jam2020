229c144d7cd6d375280625f5f1cd7443802e4e89
stathampen <33280323+stathampen@users.noreply.github.com>
2020-10-12 17:28:11 +0100

    created a turret for th etest badger prefab which targets the sun object regardless of player location, badger itself will target teh player when close

---
+++ Assets/Scripts/Enemy/BadgerEnemyBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadgerEnemyBehaviour : BaseEnemyBehaviour
+{
+    // Update is called once per frame
+    protected override void Awake() {
+        base.Awake();
+    }
+
+    // private void OnDrawGizmos() {
+    //     Gizmos.DrawSphere(transform.position, maxDistantToShootTarget);
+    // }
+}
---
+++ Assets/Scripts/Enemy/BadgerTurretBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadgerTurretBehaviour : BaseEnemyBehaviour
+{
+    [SerializeField] private float minDistantToShootTarget = 1.4f;
+    [SerializeField] private float maxDistantToShootTarget = 5f;
+
+    private GameObject shootTarget;
+    private Animator animator;
+
+
+    private void Awake() 
+    {
+        base.Awake();
+        shootTarget = base.getSunTarget();
+        animator = GetComponent<Animator>();
+
+    }
+    private void Update() 
+    {
+        transform.LookAt(shootTarget.transform);
+
+        float shootTargetDistance = Vector3.Distance(transform.position, shootTarget.transform.position);
+
+        if((shootTargetDistance > minDistantToShootTarget) && (shootTargetDistance < maxDistantToShootTarget))
+        {
+            animator.SetTrigger("FireTurret");
+        }
+    }
+
+    public void FireBullet()
+    {
+        Debug.Log("Firing Bullet");
+    }
+}
