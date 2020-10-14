804711e0a5d11062bb6985b3ee05865f5bd1618e
Jonathan <jonathan@carter.games>
2020-10-14 13:59:03 +0100

    Menu Portal + Doors

--- Assets/Scripts/Menus & UI/MenuGateScript.cs
+++ Assets/Scripts/Menus & UI/MenuGateScript.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 /*
@@ -12,22 +13,38 @@ namespace DresslikeaGnome.OhGnomes
     public class MenuGateScript : MonoBehaviour
     {
         [SerializeField] private bool shouldChangeScene;
+        [SerializeField] private bool shouldQuitGame;
         [SerializeField] private string sceneName;
+        [SerializeField] private GameObject UI;
+
+        [SerializeField] private Animator anim;
 
 
         private void OnTriggerEnter(Collider other)
         {
+            anim.SetTrigger("OpenDoor");
+
             if (other.gameObject.CompareTag("Player"))
             {
                 if (shouldChangeScene)
                 {
                     SceneManager.LoadSceneAsync(sceneName);
                 }
+                else if (shouldQuitGame)
+                {
+                    Application.Quit();
+                }
                 else
                 {
-
+                    UI.SetActive(true);
                 }
             }
+        }
+
+
+        private void OnTriggerExit(Collider other)
+        {
+            anim.SetTrigger("CloseDoor");
         }
     }
 }
