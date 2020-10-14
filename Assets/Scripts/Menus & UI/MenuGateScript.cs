9afdb2891da5af3bd40405ab16eccba964199fb6
Jonathan <jonathan@carter.games>
2020-10-14 12:40:25 +0100

    Main menu work - stopping for lunch
    some work on the main menu scene, got distacted by a phone call and the stress afterwards of knowing I have an interview later in the week. Will get back at it after a break for lunch

---
+++ Assets/Scripts/Menus & UI/MenuGateScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+*  Copyright (c) Jonathan Carter
+*  E: jonathan@carter.games
+*  W: https://jonathan.carter.games/
+*/
+
+namespace DresslikeaGnome.OhGnomes
+{
+    public class MenuGateScript : MonoBehaviour
+    {
+        [SerializeField] private bool shouldChangeScene;
+        [SerializeField] private string sceneName;
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                if (shouldChangeScene)
+                {
+                    SceneManager.LoadSceneAsync(sceneName);
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
