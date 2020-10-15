737737b1099978a4cc71871dd27dd7d4f8951a2a
stathampen <33280323+stathampen@users.noreply.github.com>
2020-10-15 15:56:23 +0100

    minor changes to the main level for testing and set up the main round controller to do a transition when the game is won. Needs setting up to to the right ending scene

--- Assets/Scripts/Menus & UI/BaseRoundController.cs
+++ Assets/Scripts/Menus & UI/BaseRoundController.cs
@@ -2,158 +2,156 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseRoundController : MonoBehaviour
+
+namespace DresslikeaGnome.OhGnomes
 {
-    
-    [SerializeField] private GameObject ratPrefab;
-    [SerializeField] private GameObject badgerPrefab;
-    [SerializeField] private MainRoundController roundControllerObject;
-
-    [SerializeField] private List<GameObject> spawnerLocations;
-    [SerializeField] private GameObject EnemyContainerArray;
-
-    private int currentWave = 0;
-    private Wave currentWaveInfo;
-    private bool checkEnemyArray = false;
-
-    public float spawnEverySeconds = 10.0f;
-    public List<Wave> waves;
-
-    public List<GameObject> spawners;
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class BaseRoundController : MonoBehaviour
     {
-        Debug.Log("start Round, waves: " + waves.Count);
-
-        //autograb the arrays needed
-
-        RunWave();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //
-        if(checkEnemyArray)
-        {
-            CheckEnemyArray();
-        }
-    }
-
-    private void RunWave()
-    {
-        checkEnemyArray = false;
-
-        Debug.Log("current wave is: " + currentWave);
-
-        //get the current wave
-        currentWaveInfo = waves[currentWave];
         
-        StartSpawning();
-    }
+        [SerializeField] private GameObject ratPrefab;
+        [SerializeField] private GameObject badgerPrefab;
+        [SerializeField] private MainRoundController roundControllerObject;
 
-    private void NextWave()
-    {
-        //incriment the round count
-        currentWave++;
-        Debug.Log("next is " + currentWave);
+        [SerializeField] private List<GameObject> spawnerLocations;
+        [SerializeField] private GameObject EnemyContainerArray;
 
-        if(currentWave < waves.Count)
+        private int currentWave = 0;
+        private Wave currentWaveInfo;
+        private bool checkEnemyArray = false;
+
+        public float spawnEverySeconds = 10.0f;
+        public List<Wave> waves;
+
+        public List<GameObject> spawners;
+
+
+        // Start is called before the first frame update
+        void Start()
         {
+            Debug.Log("start Round, waves: " + waves.Count);
+
+            //autograb the arrays needed
+
             RunWave();
         }
-        else
+
+        // Update is called once per frame
+        void Update()
         {
-            //completed the round!
-            EndRound();
-        }
-
-    }
-
-    private void EndWave()
-    {
-        Debug.Log("Wave complete");
-
-        //turn off the the array checker once the round has ended
-        checkEnemyArray = !checkEnemyArray;
-        //add a message forthe user
-
-        NextWave();
-    }
-
-    private void EndRound()
-    {
-        roundControllerObject.EndRound();
-    }
-
-    private void SpawnEnemy()
-    {
-        //randomly decide where the enmies will come from
-        int locationId = Random.Range(0, spawnerLocations.Count);
-
-        //randomly decide which enemy to spawn IF theres enough left to spawn
-        if (Random.Range(0,2) == 1)
-        {
-            if (currentWaveInfo.noOfRats > 0)
+            //
+            if(checkEnemyArray)
             {
-                Instantiate(ratPrefab, 
-                spawnerLocations[locationId].transform.position, 
-                Quaternion.identity, 
-                EnemyContainerArray.transform);
-
-                currentWaveInfo.noOfRats--;
-            }
-        }
-        else
-        {
-            if(currentWaveInfo.noOfBadgers > 0)
-            {
-                Instantiate(badgerPrefab, 
-                spawnerLocations[locationId].transform.position, 
-                Quaternion.identity, 
-                EnemyContainerArray.transform);
-
-                currentWaveInfo.noOfBadgers--;
+                CheckEnemyArray();
             }
         }
 
-        //once all badgers + rats have been spawned theres no need to keep calling this function
-        if(currentWaveInfo.noOfBadgers == 0 && currentWaveInfo.noOfRats == 0)
+        private void RunWave()
         {
-            StopSpawning();
-            checkEnemyArray = true;
-        }
-        
-    }
+            checkEnemyArray = false;
 
-    private void CheckEnemyArray()
-    {
-        if(EnemyContainerArray.transform.childCount == 0)
+            //get the current wave
+            currentWaveInfo = waves[currentWave];
+            
+            StartSpawning();
+        }
+
+        private void NextWave()
         {
-            Debug.Log("ending wave: " + currentWave);
+            //incriment the round count
+            currentWave++;
 
-            EndWave();
+            //because of how arrays are counts (item 1 = array spot 0) have to look one below
+            if(currentWave < waves.Count)
+            {
+                RunWave();
+            }
+            else
+            {
+                //completed the round!
+                EndRound();
+            }
+
+        }
+
+        private void EndWave()
+        {
+            //turn off the the array checker once the round has ended
+            checkEnemyArray = !checkEnemyArray;
+            //add a message forthe user
+
+            NextWave();
+        }
+
+        private void EndRound()
+        {
+            roundControllerObject.EndRound();
+        }
+
+        private void SpawnEnemy()
+        {
+            //randomly decide where the enmies will come from
+            int locationId = Random.Range(0, spawnerLocations.Count);
+
+            //randomly decide which enemy to spawn IF theres enough left to spawn
+            if (Random.Range(0,2) == 1)
+            {
+                if (currentWaveInfo.noOfRats > 0)
+                {
+                    Instantiate(ratPrefab, 
+                    spawnerLocations[locationId].transform.position, 
+                    Quaternion.identity, 
+                    EnemyContainerArray.transform);
+
+                    currentWaveInfo.noOfRats--;
+                }
+            }
+            else
+            {
+                if(currentWaveInfo.noOfBadgers > 0)
+                {
+                    Instantiate(badgerPrefab, 
+                    spawnerLocations[locationId].transform.position, 
+                    Quaternion.identity, 
+                    EnemyContainerArray.transform);
+
+                    currentWaveInfo.noOfBadgers--;
+                }
+            }
+
+            //once all badgers + rats have been spawned theres no need to keep calling this function
+            if(currentWaveInfo.noOfBadgers == 0 && currentWaveInfo.noOfRats == 0)
+            {
+                StopSpawning();
+                checkEnemyArray = true;
+            }
+            
+        }
+
+        private void CheckEnemyArray()
+        {
+            if(EnemyContainerArray.transform.childCount == 0)
+            {
+                EndWave();
+            }
+        }
+
+        private void StartSpawning() 
+        {
+            //after 2 seconds will start spawning the enemy every set number of seconds
+            InvokeRepeating("SpawnEnemy", 2.0f, spawnEverySeconds);
+        }
+
+        private void StopSpawning()
+        {
+            //stops spawning enemies
+            CancelInvoke("SpawnEnemy");
         }
     }
 
-    private void StartSpawning() 
+    [System.Serializable]
+    public class Wave
     {
-        //after 2 seconds will start spawning the enemy every set number of seconds
-        InvokeRepeating("SpawnEnemy", 2.0f, spawnEverySeconds);
+        public int noOfRats;
+        public int noOfBadgers;
     }
-
-    private void StopSpawning()
-    {
-        //stops spawning enemies
-        CancelInvoke("SpawnEnemy");
-    }
-}
-
-[System.Serializable]
-public class Wave
-{
-    public int noOfRats;
-    public int noOfBadgers;
 }
