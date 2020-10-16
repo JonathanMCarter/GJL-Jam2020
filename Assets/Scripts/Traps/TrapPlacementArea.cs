7932841be7908b95e7f8594de3e04508c4b28c17
Jonathan <jonathan@carter.games>
2020-10-16 16:46:02 +0100

    Trap UI (WIP)

--- Assets/Scripts/Traps/TrapPlacementArea.cs
+++ Assets/Scripts/Traps/TrapPlacementArea.cs
@@ -12,5 +12,21 @@ namespace DresslikeaGnome.OhGnomes
     {
         public TrapTypes currentTrap;
         public bool hasTrap;
+
+
+        private void Start()
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            GetComponent<MeshRenderer>().enabled = true;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+        }
     }
 }
