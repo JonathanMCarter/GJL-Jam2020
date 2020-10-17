531d4bb64f1d06c4fb330ae72890a95e34c80ff0
Jonathan <jonathan@carter.games>
2020-10-17 21:35:12 +0100

    Footstep Sounds (need a little tweaking, but a start)

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+/*
+*  Copyright (c) Jonathan Carter
+*  E: jonathan@carter.games
+*  W: https://jonathan.carter.games/
+*/
+
+namespace DresslikeaGnome.OhGnomes
+{
+    public class Footsteps : MonoBehaviour
+    {
+        [SerializeField] private AudioClip[] footsteps;
+        [SerializeField] private GameObject soundPrefab;
+
+        public void PlayFootstep()
+        {
+            GameObject clip = Instantiate(soundPrefab);
+            clip.GetComponent<AudioSource>().clip = footsteps[Random.Range(0,7)];
+            clip.GetComponent<AudioSource>().volume = Random.Range(.8f, 1f);
+            clip.GetComponent<AudioSource>().pitch = Random.Range(.9f, 1.1f);
+            clip.GetComponent<AudioSource>().Play();
+            Destroy(clip, clip.GetComponent<AudioSource>().clip.length);
+        }
+    }
+}
